cc7fa36d1719c99057916c87724772398396f3e4
rinorezky <syahfrinorezky@gmail.com>
2025-10-15 08:52:52 +0800

    feat: add alert components

---
+++ app/Views/components/alert.php
@@ -0,0 +1,29 @@
+<?php
+
+$success = session()->getFlashdata('success');
+$error = session()->getFlashdata('error');
+
+?>
+
+<?php if ($error) : ?>
+    <div class="alert py-2 px-3 flex items-center space-x-2 border border-red-700 bg-red-200 rounded-lg">
+        <i class="fas fa-exclamation-circle text-red-700 text-sm"></i>
+        <p class="text-red-700"><?= esc($error) ?></p>
+    </div>
+<?php endif; ?>
+
+<?php if ($success) : ?>
+    <div class="alert py-2 px-3 flex items-center space-x-2 border border-green-500 bg-green-200 rounded-lg">
+        <i class="fas fa-check-circle text-green-700 text-sm"></i>
+        <p class="text-green-700"><?= esc($success) ?></p>
+    </div>
+<?php endif; ?>
+
+
+<script>
+    setTimeout(() => {
+        document.querySelectorAll('.alert').forEach(alert => {
+            alert.style.display = 'none';
+        });
+    }, 5000);
+</script>
