de585dac4da272b4cfcdf6f68e12f04e6dc82d5c
rinorezky <syahfrinorezky@gmail.com>
2025-10-15 08:51:11 +0800

    feat: add user rules

--- app/Validation/UserRules.php
+++ app/Validation/UserRules.php
@@ -4,8 +4,28 @@ namespace App\Validation;
 
 class UserRules
 {
-    // public function custom_rule(): bool
-    // {
-    //     return true;
-    // }
+    public $registration = [
+        'nama_lengkap' => [
+            'rules' => 'required|min_length[1]',
+            'errors' => [
+                'required' => 'Nama lengkap dibutuhkan',
+                'min_length' => 'Nama lengkap harus terdiri dari minimal 1 karakter',
+            ]
+        ],
+        'email' => [
+            'rules' => 'required|valid_email|is_unique[users.email]',
+            'errors' => [
+                'required' => 'Email dibutuhkan',
+                'valid_email' => 'Email tidak valid',
+                'is_unique' => 'Email sudah terdaftar',
+            ]
+        ],
+        'password' => [
+            'rules' => 'required|min_length[8]',
+            'errors' => [
+                'required' => 'Password dibutuhkan',
+                'min_length' => 'Password harus terdiri dari minimal 8 karakter',
+            ]
+        ]
+    ];
 }
