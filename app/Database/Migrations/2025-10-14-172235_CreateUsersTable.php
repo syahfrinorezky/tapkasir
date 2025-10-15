d35b88887013498d15d09ae7fa968e656d31e63d
rinorezky <syahfrinorezky@gmail.com>
2025-10-15 08:50:51 +0800

    feat: add users table

--- app/Database/Migrations/2025-10-14-172235_CreateUsersTable.php
+++ app/Database/Migrations/2025-10-14-172235_CreateUsersTable.php
@@ -8,11 +8,58 @@ class CreateUsersTable extends Migration
 {
     public function up()
     {
-        
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama_lengkap' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => false,
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+                'null'       => false,
+                'unique'     => true,
+            ],
+            'password' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => false,
+            ],
+            'role' => [
+                'type'       => 'ENUM',
+                'constraint' => ['admin', 'cashier'],
+                'default'    => 'cashier',
+                'null'       => false,
+            ],
+
+            // timestamps
+            'updated_at' => [
+                'type'       => 'DATETIME',
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type'       => 'DATETIME',
+                'null'       => true,
+            ],
+            'deleted_at' => [
+                'type'       => 'DATETIME',
+                'null'       => true,
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+
+        $this->forge->createTable('users');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('users');
     }
 }
