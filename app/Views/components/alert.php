681ce138305af2a37db19b5d8668c0156a7e883b
rinorezky <syahfrinorezky@gmail.com>
2025-10-22 10:41:16 +0800

    chore: add text small style

--- app/Views/components/alert.php
+++ app/Views/components/alert.php
@@ -8,14 +8,14 @@ $error = session()->getFlashdata('error');
 <?php if ($error) : ?>
     <div class="alert py-2 px-3 flex items-center space-x-2 border border-red-700 bg-red-200 rounded-lg">
         <i class="fas fa-exclamation-circle text-red-700 text-sm"></i>
-        <p class="text-red-700"><?= esc($error) ?></p>
+        <p class="text-red-700 text-sm"><?= esc($error) ?></p>
     </div>
 <?php endif; ?>
 
 <?php if ($success) : ?>
     <div class="alert py-2 px-3 flex items-center space-x-2 border border-green-500 bg-green-200 rounded-lg">
         <i class="fas fa-check-circle text-green-700 text-sm"></i>
-        <p class="text-green-700"><?= esc($success) ?></p>
+        <p class="text-green-700 text-sm"><?= esc($success) ?></p>
     </div>
 <?php endif; ?>
 
