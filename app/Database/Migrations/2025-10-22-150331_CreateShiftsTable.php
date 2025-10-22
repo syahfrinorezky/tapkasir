de72c9bf61248986accca1b957903e060a3e6ead
rinorezky <syahfrinorezky@gmail.com>
2025-10-22 23:25:41 +0800

    feat: add shifts table

---
+++ app/Database/Migrations/2025-10-22-150331_CreateShiftsTable.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateShiftsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'start_time' => [
+                'type' => 'TIME',
+            ],
+            'end_time' => [
+                'type' => 'TIME',
+            ],
+            
+            //timestamps 
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+
+        $this->forge->createTable('shifts');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('shifts');
+    }
+}
