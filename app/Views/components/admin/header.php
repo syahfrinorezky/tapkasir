680d1a66c49a8e96d70b791564e0ff764e6e5f6d
rinorezky <syahfrinorezky@gmail.com>
2025-10-22 10:42:33 +0800

    chore: hidden header for desktop view

--- app/Views/components/admin/header.php
+++ app/Views/components/admin/header.php
@@ -1,5 +1,5 @@
-<div x-data="{ slideOpen: false }" class="flex items-center justify-between px-4 py-3 bg-white shadow-md w-full">
-    <a href="<?= base_url('admin/beranda') ?>" class="flex items-center space-x-2">
+<div x-data="{ slideOpen: false }" class="flex lg:hidden items-center justify-between px-4 py-3 bg-white shadow-md w-full">
+    <a href="<?= base_url('admin/dashboard') ?>" class="flex items-center space-x-2">
         <img src="<?= base_url('images/logo/tapkasir.png') ?>" alt="TapKasir" class="w-12 h-12">
         <h1 class="hidden md:flex font-primary text-lg md:text-xl font-semibold">TapKasir</h1>
     </a>
