ca69faa832ebced7530513968dbf23d7093263bf
rinorezky <syahfrinorezky@gmail.com>
2025-10-22 10:44:08 +0800

    feat: add account view with logout modal

--- app/Views/components/admin/sidebar.php
+++ app/Views/components/admin/sidebar.php
@@ -1,11 +1,45 @@
-<div class="hidden lg:flex lg:w-1/5 bg-white shadow-md h-screen overflow-hidden">
-    <div class="flex flex-col w-full space-y-5 font-secondary p-5">
-        <h1 class="font-bold font-primary text-xl">Dashboard Admin</h1>
-        <nav class="flex flex-col space-y-4">
-            <a href="<?= base_url('admin/beranda') ?>" class="px-3 py-4 flex items-center space-x-2 hover:bg-gray-200 hover:font-semibold rounded-md transition-all duration-300 ease-in-out <?= uri_string() == 'admin/beranda' ? 'bg-gray-200 font-medium' : '' ?>">
-                <i class="fas fa-home text-primary"></i>
-                <span>Beranda</span>
-            </a>
-        </nav>
+<div class="hidden lg:flex w-1/5 bg-white shadow-md h-screen">
+    <div class="overflow-hidden w-full flex flex-col justify-between p-4">
+        <a href="<?= base_url('admin/dashboard') ?>" class="flex items-center justify-center space-x-2 py-4">
+            <img src="<?= base_url('images/logo/tapkasir.png') ?>" alt="TapKasir" class="w-14 h-14 object-contain">
+            <span class="hidden md:inline font-primary font-extrabold text-gray-700 text-xl">TapKasir</span>
+        </a>
+        <div class="flex flex-col w-full space-y-5 font-secondary  overflow-y-auto h-screen">
+            <nav class="flex flex-col space-y-4">
+                <a href="<?= base_url('admin/dashboard') ?>" class="px-8 py-4 flex items-center space-x-3 hover:bg-secondary hover:font-semibold rounded-md transition-all duration-300 ease-in-out <?= uri_string() == 'admin/dashboard' ? 'bg-secondary text-gray-800 font-semibold' : 'text-gray-500' ?>">
+                    <i class="fas fa-home text-primary text-xl"></i>
+                    <span>Dashboard</span>
+                </a>
+            </nav>
+        </div>
+        <div class="flex items-center justify-between p-4">
+            <div class="flex space-x-3">
+                <div class="p-2.5 bg-gray-200 rounded-full">
+                    <i class="fas fa-user text-gray-600"></i>
+                </div>
+                <div class="flex flex-col justify-center">
+                    <?php
+                    $nama = session()->get('nama_lengkap') ?? 'Admin';
+                    $maxLength = 15;
+                    $displayNama = mb_strlen($nama) > $maxLength ? mb_substr($nama, 0, $maxLength) . '...' : $nama;
+                    ?>
+                    <span class="font-secondary text-sm text-gray-700"><?= esc($displayNama) ?></span>
+
+                    <span class="text-gray-400 text-xs capitalize"><?= session()->get('role') ?? 'Admin' ?></span>
+                </div>
+            </div>
+            <div x-data="{ isAccMenuOpen: false }" class="relative">
+                <button @click="isAccMenuOpen = !isAccMenuOpen" type="button" class="flex items-center justify-center w-8 h-8 hover:bg-gray-200 rounded-md cursor-pointer transition-colors duration-300 ease-in-out">
+                    <i class="fas fa-ellipsis text-gray-600 text-sm"></i>
+                </button>
+
+                <div x-show="isAccMenuOpen" @click.away="isAccMenuOpen = false" x-transition class="absolute bottom-14 -translate-x-3/4 w-48 bg-white shadow-md rounded-md z-50">
+                    <a href="<?= base_url('logout') ?>" class="flex items-center px-4 py-3 hover:bg-gray-100 rounded-md transition-colors duration-300 ease-in-out">
+                        <i class="fas fa-sign-out-alt text-red-500 mr-2"></i> 
+                        <span class="text-gray-500 text-sm">Logout</span>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
