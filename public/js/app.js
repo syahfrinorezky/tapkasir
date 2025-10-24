3c68c691d2f238352fb3e735678109342648a59b
rinorezky <syahfrinorezky@gmail.com>
2025-10-24 13:37:19 +0800

    chore: add chart js library

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,9 @@
 import Alpine from "alpinejs";
 import "@fortawesome/fontawesome-free/js/all.js";
+import { Chart, registerables } from "chart.js";
 
+Chart.register(...registerables);
+window.Chart = Chart;
 window.Alpine = Alpine;
 
 Alpine.start();
